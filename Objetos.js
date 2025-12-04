2bc5e5eeff676184d707b2bea54c89a0c7bc15e8
Jehovan <jehovanpro841@gmail.com>
2025-12-03 19:55:50 -0600

    Apuntes de Objetos

---
+++ Objetos.js
@@ -0,0 +1,17 @@
+/*let arreglo = [{nombre:"dany", edad:29, peso:85, altura:1.70}, {nombre:"jehovan", edad:18, peso:62, altura:1.60}];
+arreglo.map(personas =>
+    console.log(`El nombre es: ${personas.nombre} y su edad es: ${personas.edad}`)
+);*/
+
+const prompt=require("prompt-sync")();
+let nombre = "";
+let edad = "";
+let datos = [];
+let personas = "";
+nombre=prompt("Ingresa el nombre: ");
+edad=parseInt(prompt("Ingresa tu edad: "));
+datos.push (personas); //para guardar los datos
+console.log (personas);
+
+
+
